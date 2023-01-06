6c8eed8e69efb1f5008f5d83d9415fc84100fba3
keerthivasan44 <100290230+keerthivasan44@users.noreply.github.com>
2023-01-06 20:05:51 +0530

    Add files via upload

---
+++ Java Script/DarkMode.js
@@ -0,0 +1,23 @@
+//toggle button
+function myFunction() {
+  var element1 = document.body;
+  var element2 = document.getElementById("projects");
+  var element3 = document.getElementById("work-experience");
+  var element4 = document.getElementById("content-wrap");
+  var element5 = document.getElementById("education");
+  var element6 = document.getElementById("content-wrap-top");
+  var element7 = document.getElementById("footer");
+  var element8 = document.getElementById("header");
+  var element9 = document.getElementById("navbar");
+  var element0 = document.getElementById("reverse");
+  element1.classList.toggle("dark-mode");
+  element2.classList.toggle("dark-mode");
+  element3.classList.toggle("dark-mode");
+  element4.classList.toggle("dark-mode");
+  element5.classList.toggle("dark-mode");
+  element6.classList.toggle("dark-mode");
+  element7.classList.toggle("dark-mode");
+  element8.classList.toggle("dark-mode");
+  element9.classList.toggle("dark-mode");
+  element0.classList.toggle("dark-mode");
+}
